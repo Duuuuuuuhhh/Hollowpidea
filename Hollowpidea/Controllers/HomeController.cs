b9168ba3314735df0a2bce2c9df16f2806298c61
Eduardo Simionato Nogueira <edunogueira793@gmail.com>
2023-05-02 08:52:31 -0300

    Alteração no Controllers

--- Hollowpidea/Controllers/HomeController.cs
+++ Hollowpidea/Controllers/HomeController.cs
@@ -1,21 +1,32 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Hollowpidea.Models;
+using Hollowpidea.Services;
 
 namespace Hollowpidea.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IHollowService _hollowService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IHollowService hollowService)
     {
         _logger = logger;
+        _hollowService = hollowService;
     }
 
-    public IActionResult Index()
+    public IActionResult Index(string tribo)
     {
-        return View();
+        var hollows = _hollowService.GetHollowpideaDto();
+        ViewData["filter"] = string.IsNullOrEmpty(tribo) ? "all" : tribo;
+        return View(hollows);
+    }
+
+    public IActionResult Details(int Numero)
+    {
+        var Personagem = _hollowService.GetDetailedPersonagem(Numero);
+        return View(Personagem);
     }
 
     public IActionResult Privacy()
@@ -26,6 +37,7 @@ public class HomeController : Controller
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id 
+                    ?? HttpContext.TraceIdentifier });
     }
 }
--- Hollowpidea/Program.cs
+++ Hollowpidea/Program.cs
@@ -9,3 +9,24 @@ builder.Services.AddHttpContextAccessor();
 builder.Services.AddSingleton<IHollowService, hollowservice>();
 
 var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseSession();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
